2a7ce286fbce4ffa06de83bcd842684139206bb7
aakkado <liltenshidesu@gmail.com>
2024-01-18 11:46:26 -0300

    initial commit

---
+++ product-service/src/test/java/com/aakkado/productservice/ProductServiceApplicationTests.java
@@ -0,0 +1,28 @@
+package com.aakkado.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+class ProductServiceApplicationTests {
+
+	@Container
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@DynamicPropertySource
+	static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry){
+		dynamicPropertyRegistry.add("spring.data.mongodb.uri",
+				mongoDBContainer::getReplicaSetUrl);
+	}
+
+	@Test
+	void contextLoads() {
+	}
+
+}
